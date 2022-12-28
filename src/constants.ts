a2468d474f23caf946c61bb716e816a1a243e9d0
Attacktive <66462458+Attacktive@users.noreply.github.com>
2022-12-28 14:12:45 +0900

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const apiRoot = "http://localhost:8080";
+export const apiRoot = "https://ec2-54-180-154-156.ap-northeast-2.compute.amazonaws.com:8080";
 export const gitHub = "https://github.com/Attacktive";
